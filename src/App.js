91266d30bedccf55abcd3b3fabd78be1e8557b56
Jon Iler <jon.iler@dfo.global>
2019-05-08 12:39:01 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+	Component
+} from 'react';
+import axios from 'axios';
+import './App.scss';
+import Search from './components/Search/Search';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+	state = {
+		artistSearch: '',
+		artistResult: ''
+	};
+
+	// updates our state with the artist to search from user input
+	searchHandler = (event) => {
+		const newSearchTerm = event.target.value;
+		this.setState({
+			artistSearch: newSearchTerm,
+		});
+	}
+
+	// calls API and stores results in state
+	submitHandler = e => {
+		e.preventDefault();
+		const artistURL = `https://theaudiodb.com/api/v1/json/195003/search.php?s=${this.state.artistSearch}`;
+		let searchResult;
+		const state = this;
+		
+		axios.get(artistURL).then((result) => {
+			console.log('response.JSON:', {
+				message: 'Request received',
+				data: result.data.artists
+			})
+			searchResult =  result.data.artists;
+			state.setState({
+				artistResult: searchResult
+			})
+		})
+	};
+
+	render() {
+		return ( 
+			<div className = "App" >
+				<h1>ARTISTA</h1>
+				<Search searchHandler={this.searchHandler} submitHandler={this.submitHandler}/>
+				<p>Click on the search button, or just hit enter. <span role="img" aria-label="SickNasty">😎</span></p>
+			</div>
+		)
+	}
 }
 
 export default App;
---
+++ src/components/Search/Search.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Search = (props) => {
+    return(
+        <form onSubmit={props.submitHandler} className="Search">
+            <input 
+                onChange={props.searchHandler} 
+                type="text" 
+                name="artistSearchField" 
+                id="artistSearchField"
+                placeholder="Enter an artist..."
+                />
+            <button type="submit"><span role="img" aria-label="search">&#128270;</span></button>
+        </form>
+    )
+}
+
+export default Search;
